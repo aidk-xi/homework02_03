e152d616e3fb3c1b2d98d682bad3f7f10edf2e69
Горячева Елизавета Александровна <goryacheva-ea@ranepa.ru>
2023-08-11 17:59:27 +0300

    Initial

---
+++ Homework02_03/Homework02_03.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+struct address
+{
+	std::string City;
+	std::string Street;
+	int HouseNumber{};
+	int ApartNumber{};
+	int Index{};
+};
+
+int main ()
+{ 
+	setlocale(LC_ALL, "Russian");
+
+	address add;
+
+	std::cout << "Город: ";
+	std::cin >> add.City;
+
+	std::cout << "Улица: "; 
+	std::cin >> add.Street;
+
+	std::cout << "Номер дома: "; 
+	std::cin >> add.HouseNumber; 
+	
+
+	std::cout << "Номер квартиры: ";
+	std::cin >> add.ApartNumber;
+
+	std::cout << "Индекс: ";
+	std::cin >> add.Index;
+
+	return 0;
+}
